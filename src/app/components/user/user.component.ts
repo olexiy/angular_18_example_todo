8ce21a69851b309a59ff238149b332cc0262bfcc
Olexiy Sokurenko <sokurenko-extern@gvl.de>
2024-09-15 20:48:02 +0200

    Project init

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-users';
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+}
